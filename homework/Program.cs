c2e03deca1d0db0601c3df775e181456c17ec52d
Максим <maxibredikhin@gmail.com>
2024-02-03 22:29:07 +0400

    Сделал дз на лекции

--- homework/Program.cs
+++ homework/Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿int d = 5;
+int c = 6;
+int a = 4;
+int b = 11;
+int sum = c + d;
+//Console.WriteLine(sum);
+
+int product1 = a * b;
+//Console.WriteLine(product1);
+
+int result = product1 / sum;
+Console.WriteLine(result);
